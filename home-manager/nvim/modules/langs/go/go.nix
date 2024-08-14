c377e7fb8528e9dbe1e5b0ce3a8d1246c11cbd14
Charlie Egan <charlieegan3@users.noreply.github.com>
2024-08-14 18:16:10 +0100

    Add golangci-lint ls

--- home-manager/nvim/modules/langs/go/go.nix
+++ home-manager/nvim/modules/langs/go/go.nix
@@ -1,26 +1,14 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ gopls ];
+  home.packages = with pkgs; [
+    gopls
+    golangci-lint-langserver
+  ];
 
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [ nvim-treesitter-parsers.go ];
 
-    extraLuaConfig = ''
-      local capabilities_go = require('cmp_nvim_lsp').default_capabilities()
-
-      require('lspconfig').gopls.setup {
-        capabilities = capabilities_go,
-        settings = {
-          gopls = {
-            gofumpt = true,
-            analyses = {
-              unusedparams = true,
-            },
-            staticcheck = true,
-          },
-        },
-      }
-    '';
+    extraLuaConfig = builtins.readFile ./config.lua;
   };
 }
